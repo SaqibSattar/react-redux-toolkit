c41ff3efa37631e599b0048fb41363563fe5d646
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-16 15:40:08 +0400

    extra reducers

--- redux-toolkit/features/icecream/icecreamSlice.js
+++ redux-toolkit/features/icecream/icecreamSlice.js
@@ -1,4 +1,5 @@
 const { createSlice } = require("@reduxjs/toolkit");
+const { cakeActions } = require("../cake/cakeSlice");
 
 const initialState = {
   numOfIcecreams: 10,
@@ -14,6 +15,16 @@ const icecreamSlice = createSlice({
     restocked: (state, action) => {
       state.numOfIcecreams += action.payload;
     },
+  },
+  // extraReducers: {  // old way
+  //   ['cake/ordered']: (state) => {
+  //     state.numOfIcecreams--;
+  //   },
+  // }
+  extraReducers: (builder) => {
+    builder.addCase(cakeActions.ordered, (state) => {
+      state.numOfIcecreams--;
+    });
   },
 });
 
