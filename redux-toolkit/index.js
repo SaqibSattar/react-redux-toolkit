e57191df0483cd82da5792c47830bbbbfd9dbc39
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-16 10:16:02 +0400

    iceCream slice

--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -1,5 +1,6 @@
 const store = require("./app/store");
 const { cakeActions } = require("./features/cake/cakeSlice");
+const { icecreamActions } = require("./features/icecream/icecreamSlice");
 
 console.log("Initial State ", store.getState());
 const unsubscribe = store.subscribe(() => {
@@ -10,5 +11,9 @@ store.dispatch(cakeActions.ordered(1));
 store.dispatch(cakeActions.ordered(1));
 store.dispatch(cakeActions.ordered(1));
 store.dispatch(cakeActions.restocked(3));
+store.dispatch(icecreamActions.ordered(1));
+store.dispatch(icecreamActions.ordered(1));
+store.dispatch(icecreamActions.ordered(1));
+store.dispatch(icecreamActions.restocked(3));
 
 unsubscribe();
