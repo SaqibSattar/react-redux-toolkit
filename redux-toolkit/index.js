ea6aa9e8dc4eba15b1b9e2a367ec880fb4d008f0
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-16 16:01:10 +0400

    async thunks

--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -1,6 +1,7 @@
 const store = require("./app/store");
 const { cakeActions } = require("./features/cake/cakeSlice");
 const { icecreamActions } = require("./features/icecream/icecreamSlice");
+const fetchUsers = require("./features/user/userSlice").fetchUsers;
 
 console.log("Initial State ", store.getState());
 // const unsubscribe = store.subscribe(() => {});
@@ -8,13 +9,14 @@ const unsubscribe = store.subscribe(() => {
   console.log("Updated State ", store.getState());
 });
 
-store.dispatch(cakeActions.ordered(1));
-store.dispatch(cakeActions.ordered(1));
-store.dispatch(cakeActions.ordered(1));
-store.dispatch(cakeActions.restocked(3));
-store.dispatch(icecreamActions.ordered(1));
-store.dispatch(icecreamActions.ordered(1));
-store.dispatch(icecreamActions.ordered(1));
-store.dispatch(icecreamActions.restocked(3));
+store.dispatch(fetchUsers());
+// store.dispatch(cakeActions.ordered(1));
+// store.dispatch(cakeActions.ordered(1));
+// store.dispatch(cakeActions.ordered(1));
+// store.dispatch(cakeActions.restocked(3));
+// store.dispatch(icecreamActions.ordered(1));
+// store.dispatch(icecreamActions.ordered(1));
+// store.dispatch(icecreamActions.ordered(1));
+// store.dispatch(icecreamActions.restocked(3));
 
-unsubscribe();
+// unsubscribe();
