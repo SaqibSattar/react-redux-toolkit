a7593c61ddd717cc432c57654eae38cc9266ba1a
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-16 15:23:20 +0400

    logger middleware

--- redux-toolkit/app/store.js
+++ redux-toolkit/app/store.js
@@ -2,11 +2,14 @@ const { configureStore } = require("@reduxjs/toolkit");
 const cakeReducer = require("../features/cake/cakeSlice");
 const icecreamReducer = require("../features/icecream/icecreamSlice");
 
+const { logger } = require("redux-logger");
+
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 module.exports = store;
--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -3,9 +3,10 @@ const { cakeActions } = require("./features/cake/cakeSlice");
 const { icecreamActions } = require("./features/icecream/icecreamSlice");
 
 console.log("Initial State ", store.getState());
-const unsubscribe = store.subscribe(() => {
-  console.log("Updated State ", store.getState());
-});
+const unsubscribe = store.subscribe(() => {});
+// const unsubscribe = store.subscribe(() => {
+//   console.log("Updated State ", store.getState());
+// });
 
 store.dispatch(cakeActions.ordered(1));
 store.dispatch(cakeActions.ordered(1));
