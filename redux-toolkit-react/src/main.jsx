240656fddcd937bb4071ae5f5b01156e0dc67fb5
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-19 10:49:36 +0400

    provider

--- redux-toolkit-react/src/main.jsx
+++ redux-toolkit-react/src/main.jsx
@@ -1,9 +1,13 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./app/store";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    <App />
-  </>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </>
+);
